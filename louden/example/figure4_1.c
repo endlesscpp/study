462b308e18194fdc825faacbcb9f6b44e88d666f
endlesscpp <baidengfeng@gmail.com>
2013-10-13 00:41:15 +0800

    Exercise 4.31. Rewrite the recursive-decent calculator of Figure 4.1 so that it computes both the integer and floating-point number

--- louden/example/figure4_1.c
+++ louden/example/figure4_1.c
@@ -1,5 +1,11 @@
 
 /*
+ * revision 2
+ * Exercise 4.31
+ * Can calculate both the integer and floating-point numbers
+ */
+
+/*
  * revision 1
  * Exercise 4.30
  * rewrite the calculator to so that it computes with floating-point instead of integers
@@ -21,13 +27,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 char token; /* global token variable */
 
+enum _e_type
+{
+    INTEGER,
+    FLOAT,
+};
+
+struct _t_value
+{
+    enum _e_type type;
+
+    union
+    {
+        int     iVal;
+        float   fVal;
+    } val;
+};
+        
+
 /* function prototypes for recursive calls */
-float expr(void);
-float term(void);
-float factor(void);
+struct _t_value expr(void);
+struct _t_value term(void);
+struct _t_value factor(void);
 
 void error(void)
 {
@@ -43,9 +68,85 @@ void match(char expectedToken)
         error();
 }
 
+static void printTValue( struct _t_value v )
+{
+    if (v.type == INTEGER)
+        printf("Result = %d\n", v.val.iVal);
+    else /* floating-point number */
+        printf("Result = %f\n", v.val.fVal);
+}
+
+float getVal(struct _t_value v)
+{
+    if (v.type == INTEGER)
+        return v.val.iVal;
+    else
+        return v.val.fVal;
+}
+
+static enum _e_type getType(struct _t_value v)
+{
+    return v.type;
+}
+
+static struct _t_value addVal(struct _t_value v1, struct _t_value v2)
+{
+    struct _t_value ret;
+
+    if (getType(v1) == INTEGER && getType(v2) == INTEGER)
+    {
+        ret.type = INTEGER;
+        ret.val.iVal = v1.val.iVal + v2.val.iVal;
+    }
+    else
+    {
+        ret.type = FLOAT;
+        ret.val.fVal = getVal(v1) + getVal(v2);
+    }
+
+    return ret;
+}
+
+static struct _t_value subVal(struct _t_value v1, struct _t_value v2)
+{
+    struct _t_value ret;
+
+    if (getType(v1) == INTEGER && getType(v2) == INTEGER)
+    {
+        ret.type = INTEGER;
+        ret.val.iVal = v1.val.iVal - v2.val.iVal;
+    }
+    else
+    {
+        ret.type = FLOAT;
+        ret.val.fVal = getVal(v1) - getVal(v2);
+    }
+
+    return ret;
+}
+
+static struct _t_value mulVal(struct _t_value v1, struct _t_value v2)
+{
+    struct _t_value ret;
+
+    if (getType(v1) == INTEGER && getType(v2) == INTEGER)
+    {
+        ret.type = INTEGER;
+        ret.val.iVal = v1.val.iVal * v2.val.iVal;
+    }
+    else
+    {
+        ret.type = FLOAT;
+        ret.val.fVal = getVal(v1) * getVal(v2);
+    }
+
+    return ret;
+}
+
+
 int main(void)
 {
-    float result;
+    struct _t_value result;
 
     /* load token with first character for lookahead */
     token = getchar();
@@ -53,16 +154,16 @@ int main(void)
     result = expr();
     
     if (token == '\n') /* the end of line */
-        printf("Result = %f\n", result);
+        printTValue(result);
     else
         error(); /* extraneous chars on line*/
 
     return 0;
 }
 
-float expr(void)
+struct _t_value expr(void)
 {
-    float temp = term();
+    struct _t_value temp = term();
 
     while (token == '+' || token == '-')
     {
@@ -70,11 +171,11 @@ float expr(void)
         {
             case '+':
                 match('+');
-                temp += term();
+                temp = addVal(temp, term());
                 break;
             case '-':
                 match('-');
-                temp -= term();
+                temp = subVal(temp, term());
                 break;
         }
     }
@@ -82,39 +183,65 @@ float expr(void)
     return temp;
 }
 
-float term(void)
+struct _t_value term(void)
 {
-    float temp = factor();
+    struct _t_value temp = factor();
     while (token == '*')
     {
         match('*');
-        temp *= factor();
+        temp = mulVal(temp, factor());
     }
 
     return temp;
 }
 
-float factor(void)
+struct _t_value factor(void)
 {
-    float temp = 0;
+    struct _t_value v = {0};
     if (token == '(')
     {
         match('(');
-        temp = expr();
+        v = expr();
         match(')');
     }
     else if (isdigit(token))
     {
+        int iVal = 0;
         ungetc(token, stdin);
-        scanf("%f", &temp);
+        scanf("%d", &iVal);
+
         token = getchar(); /* get next char */
+
+        if (token == '.') /* a floating point number */
+        {
+            v.type = FLOAT;
+            v.val.iVal = iVal;
+            
+            scanf("%d", &iVal);
+            int divisor = 1;
+            int temp = iVal;
+            while (temp)
+            {
+                divisor *= 10;
+                temp /= 10;
+            }
+
+            v.val.fVal = v.val.iVal + ((float)iVal) / divisor;
+            token = getchar();
+        }
+        else
+        {
+            v.type = INTEGER;
+            v.val.iVal = iVal;
+        }
+
     }
     else
     {
         error();
     }
 
-    return temp;
+    return v;
 }
 
 
