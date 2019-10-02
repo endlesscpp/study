9d450b86bad53c453fc4e59b6db3488be706a615
endlesscpp <baidengfeng@gmail.com>
2019-10-02 23:52:59 +0800

    get the shared library path with dl_addr api

--- sample/linux/dylib/mod1.c
+++ sample/linux/dylib/mod1.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include "demo.h"
+#define _GNU_SOURCE
+#include <dlfcn.h>
+
+
+typedef struct DL_info {
+    const char* dli_fname;
+    void* dli_fbase;
+    const char* dli_sname;
+    void* dli_saddr;
+} Dl_info;
 
 void func_mod1()
 {
     printf("In func_mod1()...\n");
+    struct DL_info dlinfo = {0};
+    dladdr(func_mod1, &dlinfo);
+
+    printf("so path = %s\n", dlinfo.dli_fname);
+
 }
