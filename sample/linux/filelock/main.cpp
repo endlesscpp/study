455f72317a3d7bd74c8816f2565e1f95d9ccd875
endlesscpp <baidengfeng@gmail.com>
2019-10-02 23:56:22 +0800

    get file inode number and the named mutex with abstract domain socket

--- sample/linux/filelock/main.cpp
+++ sample/linux/filelock/main.cpp
@@ -3,7 +3,41 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/mman.h>
 #include <inttypes.h>
+
+// for socket
+#include <sys/un.h>
+#include <sys/socket.h>
+
+//
+// use abstract domain socket as the named mutex
+// -1 means failed.
+//
+static int mutexTryLock(const char* name) {
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+
+    strncpy(&addr.sun_path[1], name, sizeof(addr.sun_path) - 2);
+    int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("socket");
+        return -1;
+    }
+
+    if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        perror("bind");
+        return -1;
+    }
+    return sockfd;
+}
+
+static void mutexUnlock(int sockfd) {
+    if (sockfd != -1) {
+        close(sockfd);
+    }
+}
 
 static int lockRegion(int fd, int cmd, short type, int offset, int len) {
     struct flock lock = {0};
@@ -34,6 +68,8 @@ static int unlock(int fd, int offset, int len) {
 //static __thread int tlsLockOffset = -1;  // -1 means not locked
 static thread_local int tlsLockOffset = -1;  // -1 means not locked
 
+
+
 static void testLock(int offset) {
     if (tlsLockOffset != -1) {
         printf("already has lock, offset = %d\n", tlsLockOffset);
@@ -47,22 +83,29 @@ static void testLock(int offset) {
         }
     }
 
-    int fd = open("/var/lock/oak/sync.lock", O_CREAT | O_RDWR, 0644);
+    const char* filePath = "/var/lock/oak/sync.lock";
+    int fd = open(filePath, O_CREAT | O_RDWR, 0644);
     if (fd == -1) {
         perror("open lock file");
         return;
     }
 
+    struct stat fileStat;
+    if (0 != stat(filePath, &fileStat)) {
+        perror("get file stat");
+    }
+    printf("inode = %ld\n", (long)fileStat.st_ino);
+    int64_t ino = fileStat.st_ino;
+    // TODO: need flush?
+    write(fd, &ino, sizeof(ino));
+
+
+    // use the share memory file descriptor
+//    int fd = shm_open("product_name", O_RDWR | O_CREAT, 0644);
+
     int ret = wlock(fd, offset, 1);
     if (ret == -1) {
         perror("wlock");
-        printf("wlock failed, offset = %d, ret = %d\n", offset, ret);
-        return;
-    }
-
-    ret = wlock(fd, offset, 1);
-    if (ret == -1) {
-        perror("wlock2");
         printf("wlock failed, offset = %d, ret = %d\n", offset, ret);
         return;
     }
@@ -88,12 +131,19 @@ static void* threadEntry(void* args) {
 }
 
 int main() {
+    int sockfd = mutexTryLock("mutex01");
+    if (-1 == sockfd) {
+        printf("lock mutex01 failed\n");
+    }
+    mutexUnlock(sockfd);
+
+
     pthread_t thread1, thread2;
     pthread_create(&thread1, nullptr, threadEntry, (void*)1);
 //    sleep(4);
-    pthread_create(&thread2, nullptr, threadEntry, (void*)1);
+//    pthread_create(&thread2, nullptr, threadEntry, (void*)1);
 
     pthread_join(thread1, nullptr);
-    pthread_join(thread2, nullptr);
+//    pthread_join(thread2, nullptr);
     return 0;
 }
