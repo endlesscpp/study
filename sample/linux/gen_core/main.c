775c712a55cce077f1ccb03aba9f614eaec903ab
endlesscpp <baidengfeng@gmail.com>
2019-10-10 01:16:31 +0800

    3rd crash log.txt

--- sample/linux/gen_core/main.c
+++ sample/linux/gen_core/main.c
@@ -12,10 +12,10 @@ int main() {
     }
 
     printf("before set rlimit, rlimit.curr = %d, max = %d\n",
-           (int)lmt.rlim_cur, (int)lmt.rlim_max);
+           (int) lmt.rlim_cur, (int) lmt.rlim_max);
 
-    lmt.rlim_cur = (rlim_t)CORE_SIZE;
-    lmt.rlim_max = (rlim_t)CORE_SIZE;
+    lmt.rlim_cur = (rlim_t) CORE_SIZE;
+    lmt.rlim_max = (rlim_t) CORE_SIZE;
 
     if (setrlimit(RLIMIT_CORE, &lmt) == -1) {
         return -1;
@@ -26,7 +26,7 @@ int main() {
     }
 
     printf("after set rlimit, rlimit.curr = %d, max = %d\n",
-           (int)lmt.rlim_cur, (int)lmt.rlim_max);
+           (int) lmt.rlim_cur, (int) lmt.rlim_max);
 
     int* ptr = NULL;
     *ptr = 10;
